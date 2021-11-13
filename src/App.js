28379abe08e9971473d6ce75bad5d67853b6401c
mingyeongmo <mingyeongmp@naver.com>
2021-11-13 19:01:23 +0900

    간단한 props 활용

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hello from './Hello';
 
 function App() {
+  const name = "hi";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Hello name="경모" />
+      <Hello name="현서" />
+      <Hello name="하트" />
+      <div>{name}</div>
+    </>
   );
 }
 
---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hello = (props) => {
+    return (
+        <>
+         안녕하세요 {props.name}
+         <br />
+        </>
+    );
+};
+
+export default Hello;
