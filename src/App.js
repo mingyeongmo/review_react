31d263b1cc2f2f13dec8e320a0938c252a548aa9
mingyeongmo <mingyeongmp@naver.com>
2021-11-14 18:07:22 +0900

    useState로 바뀌는값 관리 쉬운 예제

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import Hello from './Hello';
+import Counter from './components/Counter';
+import Hello from './components/Hello';
 import Wrapper from './Wrapper';
 
 function App() {
   
   return (
     <>
-      <Wrapper>
-        <Hello name="경모" color="pink" size="1.5rem" isSpecial={true}/>
+      <Counter/>
+    
+      {/* <Wrapper>
+        <Hello name="경모" color="pink" size="1.5rem" isSpecial={true}/> */}
         {/* isSpecial만 써도 true로 간주함 */}
-        <Hello name="현서" color="blue" size="1.3rem"/>
+        {/* <Hello name="현서" color="blue" size="1.3rem"/>
         <Hello name="하트" color="red" size="1rem"/>
         <Hello />
-      </Wrapper>
+      </Wrapper> */}
     </>
   );
 }
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as S from './style/HelloSt';
+import * as S from '../style/HelloSt';
 
 const Hello = ({size, color, name, isSpecial}) => {
     return (
