202419c3b73b8b3ce21d0d89a549bc34ad5c7c03
mingyeongmo <mingyeongmp@naver.com>
2021-11-29 20:05:01 +0900

    배열에 항목 추가하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 // import Counter from './components/Counter';
 // import Hello from './components/Hello';
 // import InputSample from './components/InputSample';
 // import Wrapper from './Wrapper';
 import UserList from './components/UserList'
+import CreateUser from './components/CreateUser';
 
 function App() {
 
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
         id: 1,
         username: 'minmo',
@@ -23,16 +39,33 @@ function App() {
         username: 'bye',
         email: 'bye@naver.com'
     }
-];
+]);
 
   const nextId = useRef(4);
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
+  //setUsers(users.concat(user));
 
+    setInputs({
+      username: '',
+      email: ''
+    });
     nextId.current += 1;
-  }
+  };
   
   return (
     <>
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
       <UserList users={users} />
       {/* <InputSample /> */}
       {/* <Counter/> */}
