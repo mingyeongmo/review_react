4e6a296f2d3a7ea209b70c291783b643227af827
mingyeongmo <mingyeongmp@naver.com>
2021-12-13 16:52:58 +0900

    React router v6 use a little

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import Users from './components/Users';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home'
+import About from './components/About'
 
 
 function App() {
 
   return (
-    <>
-      <Users />;
-    </>
+    <BrowserRouter>
+    <div>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
